df15d091cbb4e9e6bb3dd32de6bc5cb0181c7830
정민기 <jmk05099@ggmail.com>
2021-11-17 18:06:05 +0900

    commit

---
+++ day13/src/SutdaCard.java
@@ -0,0 +1,24 @@
+public class SutdaCard {
+    int num;
+    boolean isKwang;
+
+    public SutdaCard(int num, boolean isKwang) {
+        this.num = num;
+        this.isKwang = isKwang;
+         }
+
+    public SutdaCard(){
+        this.num = 1;
+        this.isKwang = true;
+    }
+
+    public String info() {
+        String i = Integer.toString(num);
+        if (isKwang) {
+            return i + 'K';
+        } else {
+            return i;
+        }
+    }
+
+}
